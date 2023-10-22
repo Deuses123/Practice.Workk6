5356d6f5c722a8b957e0f7f3b26355662d3d0f11
Deuses123 <deusdeqds@gmail.com>
2023-10-22 14:44:41 +0600

    init

---
+++ Library/Class1.cs
@@ -0,0 +1,112 @@
+﻿using Library.Bankomat.Accounts;
+using Library.Bankomat.MyBank;
+using Library.Bankomat.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    namespace Bankomat
+    {
+
+
+        namespace MyBank
+        {
+
+            public class Bank
+            {
+                
+                private string accountNumber;
+                private decimal initialBalance;
+
+                public Account openAccount(Client client, decimal initialBalance)
+                {
+                    Random random = new();
+                    string accountNumber = random.Next().ToString();
+                    
+                    Account newAccount = new Account(accountNumber, initialBalance);
+
+                    client.Account = newAccount;
+
+                    Console.WriteLine($"\nСчет: {accountNumber}\nИмя: {client.Name}\nБаланс: {initialBalance}\n");
+
+                    return newAccount;
+                }
+            }
+        }
+
+        namespace Clients
+        {
+
+            public class Client
+            {
+                public string Name { get; set; }
+                public byte[] CardNumber { get; set; }
+                public string Password { get; set; }
+                public Account Account { get; set; }
+
+                public Client(string name, string password)
+                {
+                    Name = name;
+                    CardNumber = Guid.NewGuid().ToByteArray();
+                    Password = password;
+                }
+
+                public Client()
+                {
+                }
+
+               
+                public void LinkAccount(Account account)
+                {
+                    Account = account;
+                }
+            }
+        }
+
+        namespace Accounts
+        {
+
+            public class Account
+            {
+                public decimal Balance { get; private set; }
+
+                public Account(string accountNumber, decimal initialBalance)
+                {
+                    Balance = initialBalance;
+                }
+
+                public void Deposit(decimal amount)
+                {
+                    if (amount > 0)
+                    {
+                        Balance += amount;
+                        Console.WriteLine($"Пополнено {amount}\nБаланс: {Balance}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("rules: Ammount > 0.");
+                    }
+                }
+
+                public bool Withdraw(decimal amount)
+                {
+                    if (amount > 0 && Balance >= amount)
+                    {
+                        Balance -= amount;
+                        Console.WriteLine($"{amount} тг снято со счета. \nБаланс: {Balance}");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Недостаточно средств");
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Practice.Workk6/Program.cs
@@ -0,0 +1,117 @@
+﻿using Library.Bankomat.Accounts;
+using Library.Bankomat.Clients;
+using Library.Bankomat.MyBank;
+
+namespace Practice.Workk6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для открытия счета в банке, введите некоторую информацию");
+            Bank bank = new Bank();
+
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("Введите пароль:");
+            string password = Console.ReadLine();
+
+            Client client = new Client(name, password);
+
+            Console.WriteLine("Введите начальный баланс:");
+            decimal initialBalance = ReadDecimal();
+
+            Account account = bank.openAccount(client, initialBalance);
+            client.LinkAccount(account);
+
+            int attempts = 3;
+            while (attempts > 0)
+            {
+                Console.Write("Введите пароль: ");
+                string inputPassword = Console.ReadLine();
+
+
+                if (inputPassword == client.Password)
+                {
+                    ShowMenu(client);
+                    break;
+                }
+                else
+                {
+                    attempts--;
+                    if (attempts > 0)
+                    {
+                        Console.WriteLine($"Неверный пароль. У вас осталось {attempts} попыток.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Исчерпаны все попытки. Попробуйте позже.");
+                        Thread.Sleep(2500);
+                    }
+                }
+            }
+        }
+
+       
+        static decimal ReadDecimal()
+        {
+            decimal result;
+            while (!decimal.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Введите корректное число.");
+            }
+            return result;
+        }
+
+        static void ShowMenu(Client client)
+        {
+            while (true)
+            {
+                Console.WriteLine("Меню:");
+                Console.WriteLine("a. Вывод баланса на экран");
+                Console.WriteLine("b. Пополнение счета");
+                Console.WriteLine("c. Снять деньги со счета");
+                Console.WriteLine("d. Выход");
+                Console.WriteLine("e. Очистить консоль");
+
+                Console.Write("Выберите действие: ");
+                char choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 'a':
+                        Console.WriteLine($"Баланс: {client.Account.Balance}");
+                        break;
+                    case 'b':
+                        Console.Write("Введите сумму для пополнения:");
+                        decimal depositAmount = ReadDecimal();
+                        client.Account.Deposit(depositAmount);
+                        break;
+                    case 'c':
+                        Console.Write("Введите сумму:");
+                        decimal withdrawAmount = ReadDecimal();
+                        if (client.Account.Withdraw(withdrawAmount))
+                        {
+                            Console.WriteLine($"{withdrawAmount} снята со счета.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Недостаточно средств на счете или сумма снятия меньше нуля.");
+                        }
+                        break;
+                    case 'd':
+                        Console.WriteLine("Выход.");
+                        return;
+                    case 'e':
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Неверный выбор. Повторите попытку.");
+                        break;
+                }
+            }
+        }
+    }
+}
